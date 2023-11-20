3f444daf9ef1a36e281981a9a4a48d4b2ee25d74
Jeremias-Schreiner <jeremias-ush@hotmail.com>
2023-11-20 18:21:15 -0300

    Tutorial #4

--- main_character.gd
+++ main_character.gd
@@ -3,7 +3,6 @@ extends CharacterBody3D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = 4.5
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
